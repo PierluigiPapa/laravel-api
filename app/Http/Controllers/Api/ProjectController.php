d42c0c9423cf9634c311fef7ff9892a96c0bbcfb
PierluigiPapa <pierluigipapa9@gmail.com>
2024-04-04 12:07:18 +0200

    feat: add api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index (){
+        $projects = Project::all();
+
+        // return response()->json([
+        //     'name' => 'Pierluigi',
+        //     'surname' => 'Papa'
+        // ]);
+
+        return response()->json([
+            'success' => true,
+            'projects' => $projects
+        ]);
+    }
+}
