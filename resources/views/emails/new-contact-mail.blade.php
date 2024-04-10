6d92b398e01cf4ad19f028f4d0e2e56154160104
PierluigiPapa <pierluigipapa9@gmail.com>
2024-04-10 13:49:32 +0200

    feat: add NewContact.php and new-contact-mail.blade-php

---
+++ resources/views/emails/new-contact-mail.blade.php
@@ -0,0 +1,9 @@
+<h1 class="text-center">Ciao PierAdmin:</h1>
+
+<p>
+    Hai ricevuto un nuovo messaggio! Ecco i dettagli: <br>
+    Nome: {{$lead->name}}<br>
+    Email: {{$lead->email}}<br>
+    Message:<br>
+    {{$lead->message}}
+</p>
